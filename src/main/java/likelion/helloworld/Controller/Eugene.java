2b693c439b06b7fe48abb0c095ba5170005d7f24
5u0612 <egoh0612@naver.com>
2024-04-11 21:09:15 +0900

    eugene

---
+++ src/main/java/likelion/helloworld/Controller/Eugene.java
@@ -0,0 +1,16 @@
+package likelion.helloworld.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Eugene {
+
+    @GetMapping("/eugene/hello-param")
+    public String helloParam(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "Eugene";
+    }
+}
